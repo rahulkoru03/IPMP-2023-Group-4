ca9c69e7780370e5fb46a9c59a76fbe100ad88e4
rahulkoru03 <105723677+rahulkoru03@users.noreply.github.com>
2023-02-09 21:39:56 +0530

    Update 28.cpp
    Added an more efficient code for 28.cpp

--- WEEK 1/28.cpp
+++ WEEK 1/28.cpp
@@ -1,3 +1,4 @@
+/*
 vector<vector<int>> fourSum(vector<int>& nums, int target) {
     long long int sum;
     int len=nums.size();
@@ -36,3 +37,46 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
     V.erase(last,V.end());
     return V;
 }
+// Time complexity and Space complexity are both O(N^4)
+*/
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    vector<vector<int> > vect;
+    sort(nums.begin(),nums.end());
+    int ptr1,ptr2;
+    auto len=nums.size();
+    for(auto i=0; i<len-3; i++) {
+        if(i>0 && nums[i]==nums[i-1]) {
+            continue;
+        }
+        for(auto j=i+1; j<len-2; j++) {
+            if(j>i+1 && nums[j]==nums[j-1]) {
+                continue;
+            }
+            ptr1=j+1;
+            ptr2=len-1;
+            while(ptr1<ptr2) {
+                auto sum=nums[i]+nums[j]+nums[ptr1]+nums[ptr2];
+                if(sum==target) {
+                    vector<int> v;
+                    v.push_back(nums[i]);
+                    v.push_back(nums[j]);
+                    v.push_back(nums[ptr1]);
+                    v.push_back(nums[ptr2]);
+                    vect.push_back(v);
+                    ptr1++;
+                    while(ptr1<ptr2 && nums[ptr1]==nums[ptr1-1]) {
+                        ptr1++;
+                    }
+                } else if(sum>target) {
+                    ptr2--;
+                } else if(sum<target) {
+                    ptr1++;
+                }
+            }
+        }
+    }
+    return vect;
+}
+// Test cases passed : 283/292
+// Runtime error : signed integer overflow, unable to overcome this issue.
